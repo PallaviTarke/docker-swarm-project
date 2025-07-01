13c39862567b8f9292bf4e3510097c719d9ab1ce
Ubuntu <azureuser@docker-swarm-mgr-1.3f5ocdrp5b3e3bqcu0bt2q5reb.rx.internal.cloudapp.net>
2025-07-01 11:52:39 +0000

    modify files

--- redis-sentinel-entrypoint.sh
+++ redis-sentinel-entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+# Wait until redis-master is reachable
 until redis-cli -h redis-master -p 6379 ping | grep -q PONG; do
-  echo "Waiting for redis-master..."
+  echo "Waiting for redis-master to be ready..."
   sleep 2
 done
 
+# Start Redis Sentinel
 exec redis-server /etc/redis/sentinel.conf --sentinel
+
