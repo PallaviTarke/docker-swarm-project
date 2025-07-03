eea6fce9b29d1afd2c004d3590aa4cf1a8854304
PallaviTarke <tarkeshubham2006@gmail.com>
2025-07-03 03:16:29 +0000

    modifies files

--- redis-sentinel-entrypoint.sh
+++ redis-sentinel-entrypoint.sh
@@ -1,10 +1,6 @@
-#!/bin/sh
-# Wait until redis-master is reachable
-until redis-cli -h redis-master -p 6379 ping | grep -q PONG; do
-  echo "Waiting for redis-master to be ready..."
+#!/bin/bash
+until redis-cli -h 10.0.1.67 -p 6379 ping | grep -q PONG; do
+  echo "Waiting for redis-master (10.0.1.67:6379) to be ready..."
   sleep 2
 done
-
-# Start Redis Sentinel
-exec redis-server /etc/redis/sentinel.conf --sentinel
-
+exec redis-sentinel /etc/redis/sentinel.conf
